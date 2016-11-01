a2f229965aacce30d3620ce3d7aba91ee2544aab
teajeni <tea.misago@gmail.com>
2016-11-01 14:44:27 +0100

    still testing on IRepository

---
+++ src/MySqlDatabase/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text;
+using DomainModel;
+using MySqlDatabase.data.MySqlClient;
+
+namespace MySqlDatabase
+{
+    interface IRepository
+    {
+
+        //Posts 
+        public IEnumerable<Posts> GetPosts(int limit, int offset)
+
+        {
+            using (var dbPost = new stackOverFlowContext())
+            { 
+
+}
