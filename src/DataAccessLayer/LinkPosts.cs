aa77275a351a0216e2fa1a2d140e39d2b017a623
teajeni <tea.misago@gmail.com>
2016-11-02 14:54:03 +0100

    Working on  LinkPost interface

---
+++ src/DataAccessLayer/LinkPosts.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    interface LinkPosts
+    {
+        IEnumerable<LinkedList> GetLinkPost(int limit, int offset);
+        int GetNumberOfLinkPosts();
+        IEnumerable<LinkPost> FindLinkPostByPostId(int postId);
+    }
+}
