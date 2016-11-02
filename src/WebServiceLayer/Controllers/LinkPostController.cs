40b08efe7ca06882e06206744642d96df6d2c717
teajeni <tea.misago@gmail.com>
2016-11-02 13:07:40 +0100

    developing LinkpostController

---
+++ src/WebServiceLayer/Controllers/LinkPostController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebServiceLayer.Controllers
+{
+    [Route("api/[controller]")]
+    public class LinkPostController : Controller
+    {
+        // GET: api/values
+        [HttpGet]
+
+        public IEnumerable<string> Get(int page = 0, int pagesize = congfig.DefaultPageSize)
+        {
+            int limit = pagesize;
+            int offset = page * pagesize;
+            var listOfLinkToPost = _repository.GetLinkToPost(limit, offset).select(prop => modelfactory.map(prop, Url));
+
+            var totalLinkPosts = _repository.GetNumberOfLinkPosts();
+
+
+            var lastpage = totalLinkPosts;
+
+            var prev = page <= 0 ? null : Url.Link(Config.LinkPostsRoute, new { page = page - 1, pagesize });
+
+            var next = page >= lastpage ? null : Url.Link(config.LinkPostsRoute, new { page = page + 1, pagesize });
+
+            var result = new
+
+            {
+                Total = totalLinkPosts,
+                Prev = prev,
+                Nex = next,
+                Data = listOfLinkToPost
+
+            };
+            return Ok(result);
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int postid)
+        {
+            var linkToPostByPostid = _repository.FindLinkToPostByPostId(postId).Select(1p => modelFactory.map(1p, Url));
+
+            return ok(linkToPostByPostid);
+        }
+
+
+    }
+}
+
+   
+        
