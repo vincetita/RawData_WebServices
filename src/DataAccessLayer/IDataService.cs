915067cc51d2ac6872d31850dc0559dea55f242a
Malik <kyli48@hotmail.com>
2016-11-02 21:07:01 +0100

    add 3 classes IDataService, MySqlDataContext, and QuestionController

---
+++ src/DataAccessLayer/IDataService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public interface IDataService
+    {
+        IList<Questions> GetQuestions();
+        IList<History> GetHistory();
+    }
+}
---
+++ src/MySqlDatabase/MysqlDataContext.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MySqlDatabase
+{
+    public class MysqlDataContext : DbContext
+    {
+        public DbSet<Questions> Question { get; set; }
+        public DbSet<History> History { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Questions>().ToTable("questions");
+            modelBuilder.Entity<Questions>().Property(q => q.QuestionId).HasColumnName("questionid");
+            modelBuilder.Entity<Questions>().Property(q => q.PostId).HasColumnName("postid");
+            modelBuilder.Entity<Questions>().Property(q => q.Title).HasColumnName("title");
+            modelBuilder.Entity<Questions>().Property(q => q.AcceptedAnswerId).HasColumnName("acceptedanswerid");
+            modelBuilder.Entity<Questions>().Property(q => q.ClosedDate).HasColumnName("closeddate");
+
+
+            modelBuilder.Entity<History>().ToTable("history");
+            modelBuilder.Entity<History>().Property(h => h.HistoryId).HasColumnName("historyid");
+            modelBuilder.Entity<History>().Property(h => h.Keyword).HasColumnName("keyword");
+            modelBuilder.Entity<History>().Property(h => h.SearchDate).HasColumnName("searchdate");
+
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql("server=localhost;database=project_portfolio1_stackoverflow; uid=root; pwd=root");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
+
+    
+
