802d148e30d3ce778399cc39b80df2c64a256839
teajeni <tea.misago@gmail.com>
2016-11-02 14:58:56 +0100

    working on Marked Post Interface

---
+++ src/DataAccessLayer/Marked Post.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    interface Marked_Post
+    {
+        IEnumerable<post> GetAllMarkedPosts(int limit, int offset);
+        int GetNumberOfMarkedPosts();
+        bool UnMarkPost(int id);
+ 
+    }
+}
