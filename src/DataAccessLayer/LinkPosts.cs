67d66639a296bbd95993b685ac46e7097a4ee734
teajeni <tea.misago@gmail.com>
2016-11-03 14:57:47 +0100

    still working on  linkPost

--- src/DataAccessLayer/LinkPosts.cs
+++ src/DataAccessLayer/LinkPosts.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace DataAccessLayer
 {
-    interface LinkPosts
+    interface LinkPosts // working on linkPost
     {
         IEnumerable<LinkedList> GetLinkPost(int limit, int offset);
         int GetNumberOfLinkPosts();
