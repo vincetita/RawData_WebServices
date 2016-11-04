34ff7abc654b2d8811612d6e2c2f0444181ce4d3
teajeni <tea.misago@gmail.com>
2016-11-04 12:45:55 +0100

    working on LinkPost

---
+++ src/MySqlDatabase/DbContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MySqlDatabase
+{
+    public class DbContext
+    {
+
+        public DbContext() : base("Linkpost")
+
+        { 
+}
+
+        public DbSet<LinkPost > LinkPost { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+
+        { 
+modelBuilder.Entity<LinkPost>().ToTable("linkpost");
+modelBuilder.Entity<LinkPost>().Property(lp => lp.PostId).HasColumnName("postid");
+modelBuilder.Entity<LinkPost>().Property(lp => lp.LinkPostId).HasColumnName("linkpostid");
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
