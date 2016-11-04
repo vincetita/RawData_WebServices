a557281e10bd5306c164c3e8ab085400831a180d
teajeni <tea.misago@gmail.com>
2016-11-04 11:07:21 +0100

    working on DataService  for adding pages and refectiry

--- src/MySqlDatabase/DataService.cs
+++ src/MySqlDatabase/DataService.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DomainModel;
 
 
 namespace MySqlDatabase
@@ -9,6 +10,13 @@ namespace MySqlDatabase
     public class DataService : IDataService
 
     {
+
+        IList<Category> GetCategories(int page, int pagesize);
+        Category GetCategory(int id);
+        void AddCategory(Category category);
+        bool UpdateCategory(Category category);
+        bool DeleteCategory(int id);
+        int GetNumberOfCategories();
 
     }
 }
