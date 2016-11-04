cfe1f6c4dd9d107597c3a428bfcf925f512161e0
teajeni <tea.misago@gmail.com>
2016-11-04 11:31:06 +0100

    working on LinkPsot  DataServices  paging

---
+++ src/MySqlDatabase/IDataService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MySqlDatabase
+{
+    public class IDataService
+    {
+                 LinkePost
+
+        public IEnumerable<LinkPost> GetLinkToPost(int limit, int offset)
+        {
+            using (var db = new StackOverflowContext())
+            {
+                return db.LinkPosts
+                    .OrderBy(lp => lp.PostId)
+                    .Skip(offset)
+                    .Take(limit)
+                    .ToList();
+            }
+        }
+
+        public int GetNumberOfLinkPosts()
+        {
+            using (var db = new StackOverflowContext())
+            {
+                return db.LinkPosts.Count();
+            }
+        }
+
+        public IEnumerable<LinkPost> FindLinkToPostByPostId(int postId)
+        {
+            using (var db = new StackOverflowContext())
+            {
+                return db.LinkPosts
+                    .Where(lp => lp.PostId == postId)
+                    .ToList();
+            }
+        }
+
+    }
+}
