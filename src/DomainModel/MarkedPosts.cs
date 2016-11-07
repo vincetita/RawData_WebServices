f5602cf2dd6ed35d64cdea7a33d8b248963a0a2c
teajeni <tea.misago@gmail.com>
2016-11-07 12:20:49 +0100

    updating the MarkedPost DM

--- src/DomainModel/MarkedPosts.cs
+++ src/DomainModel/MarkedPosts.cs
@@ -7,35 +7,8 @@ namespace DomainModel
 {
     public class MarkedPosts
     {
-        public IHttpActionResult Get(int page = 0, int pagesize = Config.DefaultPageSize)
-        {
-            int limit = pagesize;
-            int offset = page * pagesize;
-            var markedPost = _repository.GetAllMakedPosts(limit, offset).Select(mp => ModelFactory.Map(mp, Url));
+        public int markedId { get; set; }
 
-            var totalMarkedPosts = _repository.GetNumberOfMarkedPosts();
-            var lastpage = totalMarkedPosts / pagesize;
-            var prev = page <= 0 ? null : Url.Link(Config.MarkedPostsRoute, new { page = page - 1, pagesize });
-            var next = page >= lastpage ? null : Url.Link(Config.MarkedPostsRoute, new { page = page + 1, pagesize });
-
-            var result = new
-            {
-                total = totalMarkedPosts,
-                Prev = prev,
-                Next = next,
-                Data = markedPost
-            };
-            return Ok(result);
-        }
-
-        /* Unmark Post*/
-        public IHttpActionResult Put(int id)
-        {
-            if (_repository.UnMarkPost(id) == false)
-                return NotFound();
-            return Ok();
-        }
-    }
-}
+        public int PostId { get; set; }
     }
 }
